8ca1111f76e413e42569427a555d52f2f54fe522
savaki <matt.ho@gmail.com>
2014-12-11 09:26:09 -0800

    - moar wip

--- export.go
+++ export.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
@@ -17,15 +22,39 @@ type Album struct {
 	CreatedTime string `json:"created_time"`
 }
 
-func Export(accessToken, bucketName string, finished chan struct{}) error {
+func ExportAlbums(accessToken string) ([]Album, error) {
 	albums := struct {
 		Albums []Album `json:"data"`
 	}{}
 
-	resp, err := http.Get()
+	resp, err := http.Get("https://graph.facebook.com/v2.2/beatsforboobs/albums?access_token=" + url.QueryEscape(accessToken))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(albums)
+	return albums, err
 }
 
-func ExportAlbums(accessToken, bucketName string, finished chan struct{}) {
+func ExportPhotos(accessToken, albumId string) ([]byte, error) {
+	uri := fmt.Sprintf("https://graph.facebook.com/v2.2/%s/photos?access_token%s", albumId, url.QueryEscape(accessToken))
+	resp, err := http.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	buffer := bytes.NewBuffer()
+	err = io.Copy(buffer, resp.Body)
+	return buffer.Bytes(), err
+}
+
+func Export(accessToken, bucketName string, finished chan struct{}) {
+	albums := ExportAlbums(accessToken)
+	for _, albumId := range albums {
+
+	}
 	auth := aws.EnvAuth()
 	api := s3.New(auth, aws.USEast)
 	bucket := api.Bucket(bucketName)
